130abcda70cf3cf3a7ac4265d1f25dce2890523b
SavinVladimir <61633933+SavinVladimir@users.noreply.github.com>
2022-01-04 12:01:00 +0300

    Add files via upload

---
+++ Задание - 2/Строки с пропущенными значениями/27531/solution.py
@@ -0,0 +1,17 @@
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (x <= y) and (not z == y) and (z or w):
+                    print(x, y, z, w)
+
+"""
+x y z w
+0 0 1 0
+0 0 1 1
+0 1 0 1
+1 1 0 1
+"""
+
+# Ответ ywzx
